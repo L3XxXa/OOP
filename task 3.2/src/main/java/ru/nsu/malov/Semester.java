b7b79699e313a3c729ae22b74383b7538a029dbd
L3XxXa <alexkkk@inbox.ru>
2021-10-23 21:52:52 +0700

    Fixes

--- task 3.2/src/main/java/ru/nsu/malov/Semester.java
+++ task 3.2/src/main/java/ru/nsu/malov/Semester.java
@@ -3,12 +3,27 @@ package ru.nsu.malov;
 import java.util.*;
 
 public class Semester {
-    TreeMap<String, Integer> semester = new TreeMap<>();
+    /**
+     * Field:
+     * semester - mapping of subject and grade
+     */
+    private Map<String, Integer> semester = new HashMap<>();
 
+    /**
+     * Method to add grade by subject
+     *
+     * @param grade   - grade
+     * @param subject - subject
+     */
     public void addGrade(int grade, String subject) {
         this.semester.put(subject, grade);
     }
 
+    /**
+     * Method to get all grades
+     *
+     * @return all semester grades
+     */
     public Collection<Integer> getGrades() {
         return semester.values();
     }
