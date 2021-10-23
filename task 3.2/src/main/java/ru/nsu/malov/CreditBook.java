be0bfd2a1e51d7c8bfea8686b9ab756b84c87c45
L3XxXa <alexkkk@inbox.ru>
2021-10-23 21:52:52 +0700

    Fixes

--- task 3.2/src/main/java/ru/nsu/malov/CreditBook.java
+++ task 3.2/src/main/java/ru/nsu/malov/CreditBook.java
@@ -3,40 +3,131 @@ package ru.nsu.malov;
 import java.util.ArrayList;
 import java.util.Collection;
 
-public class CreditBook{
-    private final int group;
-    private final String name;
-    private static int gradeOfQualifiedWork;
-    final private int SEMESTERS = 8;
-    private final Semester[] semester = new Semester[SEMESTERS + 1];
+public class CreditBook {
+    /**
+     * Fields:
+     * group - number of student's group
+     * name - student's name
+     * sem - number of semester
+     * gradeOfQualifyingWork - student's grade of qualifying work
+     * Constants:
+     * SEMESTERS - amount of semesters
+     * semester - array for all semesters
+     */
+    private int group;
+    private String name;
+    private int gradeOfQualifyingWork;
+    private int sem;
+    private final int SEMESTERS = 8;
+    private final Semester[] semester = new Semester[SEMESTERS];
 
+    /**
+     * Constructor - new credit book
+     *
+     * @param group       - student's group
+     * @param name        - student's name
+     * @param gradeOfWork - student's grade of qualifying work
+     */
     public CreditBook(int group, String name, int gradeOfWork) {
         this.group = group;
         this.name = name;
-        gradeOfQualifiedWork = gradeOfWork;
-        for (int i = 1; i < SEMESTERS + 1; i++) {
-            Semester sem = new Semester();
-            semester[i] = sem;
+        gradeOfQualifyingWork = gradeOfWork;
+        for (int i = 0; i < SEMESTERS; i++) {
+            semester[i] = new Semester();
         }
     }
 
-    public void setGradeOfWork(int grade) {
-        gradeOfQualifiedWork = grade;
+    /**
+     * Method to show group number
+     *
+     * @return group number
+     */
+    public int showGroup() {
+        return group;
     }
 
+    /**
+     * Method to show number of semesters
+     */
+    public int showSem() {
+        return sem;
+    }
+
+    /**
+     * Method to show student's name
+     *
+     * @return student's name
+     */
+    public String showName() {
+        return name;
+    }
+
+    /**
+     * Method to show student's grade of the qualifying work
+     *
+     * @return student's grade of the qualifying work
+     */
+    public int showGradeOfQualifyingWork() {
+        return gradeOfQualifyingWork;
+    }
+
+    /**
+     * Method to add group number
+     *
+     * @param group - group number
+     */
+    public void addGroup(int group) {
+        this.group = group;
+    }
+
+    /**
+     * Method to add student's name
+     *
+     * @param name - student's name
+     */
+    public void addName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Method to set grade of qualifying work
+     *
+     * @param grade - grade of qualifying work
+     */
+    public void setGradeOfWork(int grade) {
+        gradeOfQualifyingWork = grade;
+    }
+
+    /**
+     * Method to add all grades in semester by subject
+     *
+     * @param sem - number of semester
+     */
     public void addGrades(int sem, int grade, String subject) {
+        this.sem = sem;
         semester[sem].addGrade(grade, subject);
     }
 
+    /**
+     * Method to get all student's grades
+     *
+     * @return arraylist with all grades
+     */
     public ArrayList<Integer> getAllGrades() {
         ArrayList<Integer> grades = new ArrayList<>();
-        for (int i = 1; i < SEMESTERS + 1; i++) {
+        for (int i = 0; i < SEMESTERS; i++) {
             grades.addAll(semester[i].getGrades());
         }
         return grades;
     }
 
-    public static double averageScore(ArrayList<Integer> grades) {
+    /**
+     * Method to find an average grade
+     *
+     * @param grades - arraylist with all student's grades.
+     * @return average student's grade.
+     */
+    public double averageGrade(ArrayList<Integer> grades) {
         double avgScore = 0;
         for (int grade : grades) {
             avgScore += grade;
@@ -45,22 +136,41 @@ public class CreditBook{
         return avgScore;
     }
 
+    /**
+     * Collection with all grades in semester
+     *
+     * @return all grades in semester.
+     */
     public Collection<Integer> getSemGrades(int sem) {
         return semester[sem].getGrades();
     }
 
-    public static boolean redDiploma(CreditBook creditBook) {
+    /**
+     * Method to define can student have a red diploma
+     *
+     * @param creditBook - Student's credit book
+     * @return true if student will have a red diploma
+     */
+    public boolean redDiploma(CreditBook creditBook) {
         ArrayList<Integer> grades = creditBook.getAllGrades();
+        int fives = 0;
         for (int grade : grades) {
             if (grade < 4) {
                 return false;
             }
+            fives++;
         }
-        return !(averageScore(grades) < 4.5) && (gradeOfQualifiedWork == 5);
+        return (double) (fives / grades.size()) >= 0.75 && (gradeOfQualifyingWork == 5);
     }
 
-    public boolean scholarship(CreditBook creditBook, int semester) {
-        Collection<Integer> grades = creditBook.getSemGrades(semester);
+    /**
+     * Method to define can student have an increased scholarship
+     *
+     * @param semester - number of semester
+     * @return true if student will have an increased scholarship
+     */
+    public boolean increasedScholarship(int semester) {
+        Collection<Integer> grades = getSemGrades(semester);
         int fours = 0;
         for (int grade : grades) {
             if (grade < 4) {
@@ -74,4 +184,45 @@ public class CreditBook{
         return fours < 3;
     }
 
+    /**
+     * Method to define can student have a scholarship
+     *
+     * @param semester - number of semester
+     * @return true if student will have a scholarship
+     */
+    public boolean scholarship(int semester) {
+        Collection<Integer> grades = getSemGrades(semester);
+        for (int grade : grades) {
+            if (grade < 4) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Method to define can student have an increased scholarship by JetBrains
+     *
+     * @param semester - number of semester
+     * @return true if student will have an increased scholarship by JetBrains
+     */
+    public boolean increasedScholarshipByJetBrains(int semester) {
+        Collection<Integer> grades = getSemGrades(semester);
+        for (int grade : grades) {
+            if (grade < 5) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Method to define can student have a scholarship by JetBrains
+     *
+     * @param semester - number of semester
+     * @return true if student will have a scholarship by JetBrains
+     */
+    public boolean scholarshipByJetBrains(int semester) {
+        return increasedScholarship(semester);
+    }
 }
--- task 3.2/src/test/java/ru/nsu/malov/CreditBookTest.java
+++ task 3.2/src/test/java/ru/nsu/malov/CreditBookTest.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 class CreditBookTest {
@@ -29,31 +29,38 @@ class CreditBookTest {
     }
 
     @Test
-    public void averageScore_myAverageScore() {
+    public void averageScore_myAverageGrade() {
         ArrayList<Integer> grades = l3xxxa.getAllGrades();
-        double i = CreditBook.averageScore(grades);
-        String result;
-        DecimalFormat decimalFormat = new DecimalFormat("#.#");
-        result = decimalFormat.format(i);
-        Assertions.assertEquals("4.2", result);
+        double i = l3xxxa.averageGrade(grades);
+        Assertions.assertEquals(i, 4.1, 0.1);
+    }
+
+    @Test
+    public void showName_showGroup_showGradeOfQualifyingWork_myName() {
+        String name = l3xxxa.showName();
+        int group = l3xxxa.showGroup();
+        int grade = l3xxxa.showGradeOfQualifyingWork();
+        Assertions.assertEquals(group, 20214);
+        Assertions.assertEquals(grade, 0);
+        Assertions.assertEquals(name, "Malov A.Y.");
     }
 
     @Test
     public void redDiploma_forMe() {
         l3xxxa.setGradeOfWork(5);
-        Assertions.assertFalse(CreditBook.redDiploma(l3xxxa));
+        Assertions.assertFalse(l3xxxa.redDiploma(l3xxxa));
         l3xxxa.setGradeOfWork(4);
-        Assertions.assertFalse(CreditBook.redDiploma(l3xxxa));
+        Assertions.assertFalse(l3xxxa.redDiploma(l3xxxa));
     }
 
     @Test
     public void scholarship_forMe() {
-        Assertions.assertFalse(l3xxxa.scholarship(l3xxxa, 2));
+        Assertions.assertFalse(l3xxxa.increasedScholarship(2));
     }
 
     @Test
-    public void averageScore_redDiploma_scholarship_goodStudent() {
-        CreditBook goodStudent = new CreditBook(20214, "Zhmishenko V.A.",  0);
+    public void averageGrade_redDiploma_scholarship_goodStudent() {
+        CreditBook goodStudent = new CreditBook(20214, "Zhmishenko V.A.", 0);
         goodStudent.addGrades(1, 4, "MathAn");
         goodStudent.addGrades(1, 5, "Discrete Maths");
         goodStudent.addGrades(1, 4, "Declarative programming");
@@ -68,26 +75,49 @@ class CreditBookTest {
         goodStudent.addGrades(2, 4, "Digital platforms");
         goodStudent.setGradeOfWork(5);
         ArrayList<Integer> grades = goodStudent.getAllGrades();
-        double i = CreditBook.averageScore(grades);
-        String result;
-        DecimalFormat decimalFormat = new DecimalFormat("#.#");
-        result = decimalFormat.format(i);
-        Assertions.assertEquals("4.8", result);
-        Assertions.assertTrue(goodStudent.scholarship(goodStudent, 2));
-        Assertions.assertTrue(CreditBook.redDiploma(goodStudent));
+        double i = l3xxxa.averageGrade(grades);
+        Assertions.assertEquals(4.8, i, 0.1);
+        Assertions.assertTrue(goodStudent.increasedScholarship(2));
+        Assertions.assertTrue(goodStudent.redDiploma(goodStudent));
         goodStudent.setGradeOfWork(4);
-        Assertions.assertFalse(CreditBook.redDiploma(goodStudent));
+        Assertions.assertFalse(goodStudent.redDiploma(goodStudent));
     }
+
     @Test
     public void scholarship_goodStudent() {
-        CreditBook goodStudent = new CreditBook(20214, "Zhmishenko V.A.",  0);
+        CreditBook goodStudent = new CreditBook(20214, "Zhmishenko V.A.", 0);
         goodStudent.addGrades(1, 4, "MathAn");
         goodStudent.addGrades(1, 4, "Discrete Maths");
         goodStudent.addGrades(1, 4, "Declarative programming");
         goodStudent.addGrades(1, 5, "History");
         goodStudent.addGrades(1, 5, "OKR");
         goodStudent.addGrades(1, 5, "Imperative programming");
-        Assertions.assertFalse(goodStudent.scholarship(goodStudent, 1));
+        Assertions.assertFalse(goodStudent.increasedScholarship(1));
+    }
+
+    @Test
+    public void allMethods_newStudent() {
+        CreditBook easyMoneySniper = new CreditBook(0, null, 0);
+        easyMoneySniper.addGroup(20214);
+        easyMoneySniper.addName("Kevin Durant");
+        easyMoneySniper.setGradeOfWork(4);
+        easyMoneySniper.addGrades(1, 5, "Dribbling");
+        easyMoneySniper.addGrades(1, 5, "Shooting");
+        easyMoneySniper.addGrades(1, 5, "Positioning");
+        easyMoneySniper.addGrades(1, 4, "Passing");
+        ArrayList<Integer> grades = easyMoneySniper.getAllGrades();
+        ArrayList<Integer> gradesExpected = new ArrayList<>(Arrays.asList(5, 4, 5, 5));
+        Assertions.assertEquals(easyMoneySniper.showSem(), 1);
+        Assertions.assertEquals(easyMoneySniper.averageGrade(grades), 4.75, 0.1);
+        Assertions.assertFalse(easyMoneySniper.redDiploma(easyMoneySniper));
+        Assertions.assertEquals(grades, gradesExpected);
+        Assertions.assertTrue(easyMoneySniper.increasedScholarship(1));
+        Assertions.assertTrue(easyMoneySniper.scholarship(1));
+        Assertions.assertTrue(easyMoneySniper.scholarshipByJetBrains(1));
+        Assertions.assertFalse(easyMoneySniper.increasedScholarshipByJetBrains(1));
+        Assertions.assertEquals(easyMoneySniper.showGroup(), 20214);
+        Assertions.assertEquals(easyMoneySniper.showGradeOfQualifyingWork(), 4);
+        Assertions.assertEquals(easyMoneySniper.showName(), "Kevin Durant");
     }
 
 }
