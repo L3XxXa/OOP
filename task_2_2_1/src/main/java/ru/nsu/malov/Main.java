f2e91c5dd8b6c20bcb651ee62a739ab4c6d0dcd4
L3XxXa <alexkkk@inbox.ru>
2022-03-28 11:03:07 +0700

    Some JSON setup

--- task_2_2_1/src/main/java/ru/nsu/malov/Main.java
+++ task_2_2_1/src/main/java/ru/nsu/malov/Main.java
@@ -2,6 +2,6 @@ package ru.nsu.malov;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("WIP");
+        System.out.println("WIP\nCheck README or comment in PR");
     }
 }
