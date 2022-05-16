2b8fd7f896bb80460812bd8a00657001cea70071
L3XxXa <alexkkk@inbox.ru>
2022-05-17 00:05:52 +0700

    delete this

--- task_2_3_1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ru.nsu.malov {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ru.nsu.malov to javafx.fxml;
-    exports ru.nsu.malov;
-}
--- task_2_3_1/src/main/java/ru/nsu/malov/GameController.java
+++
@@ -1,9 +0,0 @@
-package ru.nsu.malov;
-
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
-
-public class GameController {
-
-}
