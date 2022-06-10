d74dfbcc581a3676d1fbb656b990df3a3c0ab9e1
L3XxXa <alexkkk@inbox.ru>
2022-06-10 11:17:03 +0700

    Reading config file init

--- task_2_4_1/build.gradle.kts
+++ task_2_4_1/build.gradle.kts
@@ -1,24 +1,26 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+    import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-plugins {
-    kotlin("jvm") version "1.6.21"
-}
+    plugins {
+        kotlin("jvm") version "1.6.21"
+    }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+    group = "org.example"
+    version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
+    repositories {
+        mavenCentral()
+    }
 
-dependencies {
-    testImplementation(kotlin("test"))
-}
+    dependencies {
+        testImplementation(kotlin("test"))
+        implementation("org.jetbrains.kotlin:kotlin-script-util")
+        implementation("org.jetbrains.kotlin:kotlin-compiler-embeddable")
+    }
 
-tasks.test {
-    useJUnitPlatform()
-}
+    tasks.test {
+        useJUnitPlatform()
+    }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
+    }
