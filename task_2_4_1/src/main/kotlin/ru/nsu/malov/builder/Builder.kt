165a6a23ba31be16d95f7644beba222a1e31905a
L3XxXa <alexkkk@inbox.ru>
2022-06-12 18:42:11 +0700

    Now can build and make a report

--- task_2_4_1/src/main/kotlin/ru/nsu/malov/builder/Builder.kt
+++ task_2_4_1/src/main/kotlin/ru/nsu/malov/builder/Builder.kt
@@ -3,6 +3,8 @@ package ru.nsu.malov.builder
 import org.gradle.tooling.GradleConnector
 import java.io.File
 import java.io.FileNotFoundException
+import java.nio.file.Files
+import java.nio.file.Path
 
 class Builder {
     fun buildLab(name: String, fileName: String){
@@ -10,10 +12,22 @@ class Builder {
             throw FileNotFoundException("$name doesn't have such file")
         }
         val connection = GradleConnector.newConnector().forProjectDirectory(File("./repos/$name/$fileName")).connect()
-        try{
+        connection.use { connection ->
             connection.newBuild().forTasks("test").run()
-        } finally {
-            connection.close()
         }
+        createReport(name, fileName)
+    }
+
+    private fun createReport(name: String, fileName: String){
+        if (!Files.isDirectory(Path.of("./testReports/$name/"))){
+            Files.createDirectories(Path.of("./testReports/$name/"))
+        }
+        val file = File("./testReports/$name/$fileName.html")
+        if(!file.exists()){
+            file.createNewFile()
+        }
+        val reportFile = File("./repos/$name/$fileName/build/reports/tests/test/index.html")
+        file.writeText(reportFile.readText())
+
     }
 }
